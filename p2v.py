d8f092822adacb83b9cc072964389e04772fb0b0
Jeremy Bowers <jeremyjbowers@gmail.com>
2020-09-21 14:44:39 -0400

    sort output list, remove all paths with >300 evs, improve output objects to include info about this path

--- p2v.py
+++ p2v.py
@@ -5,50 +5,62 @@ import ujson as json
 import settings
 import utils
 
+def direction_test(cand, rating):
+    if cand['condition'] == ">":
+        if rating > 0:
+            return True
+
+    if cand['condition'] == "<":
+        if rating < 0:
+            return True
+
+    return False
+
 
 sheet = utils.get_sheet(settings.SHEET_ID, settings.SHEET_RANGE)
 
-states = [{
-    "state": a['state'],
-    "ev": int(a['ev']), 
-    "pollclose": a['first_results'],
-    "90pct12hrs": utils.x_to_bool(a['90pct12hr']), 
-    "swing": utils.x_to_bool(a['swing2']), 
-    "rating": int(a['rating'])
- } for a in sheet]
-
-swing = [a for a in states if a['swing'] == True]
-
 for cand in settings.CANDIDATES:
-    def direction_test(cand, rating):
-        if cand['condition'] == ">":
-            if rating > 0:
-                return True
+    states = [{
+        "state": a['state'],
+        "ev": int(a['ev']), 
+        "pollclose": a['first_results'],
+        "90pct12hrs": utils.x_to_bool(a['90pct12hr']), 
+        "swing": utils.x_to_bool(a['swing2']), 
+        "rating": int(a['rating'])
+    } for a in sheet]
 
-        if cand['condition'] == "<":
-            if rating < 0:
-                return True
-
-        return False
+    swing = [a for a in states if a['swing'] == True]
 
     locked_states = [a for a in states if a['swing'] == False and direction_test(cand, a['rating'])]
 
     cand_evs = sum([a['ev'] for a in locked_states])
 
-    print(f"{cand['name']} has {cand_evs} locked-in EVs")
-
     paths = utils.elect_paths(swing, 270 - cand_evs)
 
-    payload = []
+    unique_paths = set([])
 
     for path in paths:
-        payload.append(path)
-        combo_string = f"{path[0]['state']} ({path[0]['ev']})"
-        for state in path[1:]:
-            combo_string += f", {state['state']} ({state['ev']})"
+        path_evs = sum([p['ev'] for p in path]) + cand_evs
+        states = sorted([f"{p['state']} ({p['ev']})" for p in path], key=lambda x: x[0])
+        state_string = ", ".join(states)
+        state_string = f"{path_evs}: {state_string}"
+        unique_paths.add(state_string)
 
-        evs = sum([p['ev'] for p in path]) + cand_evs
-        print(f"Winning path: {cand['name']} {evs} EVs via {combo_string}")
+    parsed_paths = []
+
+    for a in unique_paths:
+        path_dict = {
+            "evs": int(a.split(': ')[0]),
+            "states": [{"state": z.split(" (")[0], "ev": int(z.split(" (")[1].replace(")", ""))} for z in a.split(": ")[1].split(", ")],
+            "state_string": a.split(": ")[1]
+        }
+        path_dict['num_states'] = len(path_dict['states'])
+        if path_dict['evs'] < 300:
+            parsed_paths.append(path_dict)
+
+    sorted_paths = sorted(parsed_paths, key=lambda x: x['num_states'])
+
+    print(f"{cand['name']} has {cand_evs} locked-in EVs and {len(sorted_paths)} paths to victory")
 
     with open(f'{cand["slug"]}_paths.json', 'w') as writefile:
-        writefile.write(json.dumps(payload))
+        writefile.write(json.dumps(sorted_paths, indent=4))
